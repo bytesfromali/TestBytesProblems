4704cdf88e999d14c3832e2a9fae4bd85018da68
Mohd Ali <alimohammad2011@gmail.com>
2020-04-14 12:18:28 +0800

    Add project files.

---
+++ ProblemScenarios/Mocking/OrderService.cs
@@ -0,0 +1,30 @@
+﻿namespace ProblemScenarios.Mocking
+{
+    public class OrderService
+    {
+        private readonly IOrderStorage _storage;
+
+        public OrderService(IOrderStorage storage)
+        {
+            _storage = storage;
+        }
+
+        public int PlaceOrder(Order order)
+        {
+            var orderId = _storage.Store(order);
+
+            // Some other work
+
+            return orderId;
+        }
+    }
+
+    public class Order
+    {
+    }
+
+    public interface IOrderStorage
+    {
+        int Store(object obj);
+    }
+}
